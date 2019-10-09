6fd9130798c7570f198da45495ce71366c355f61
Aditya2223 <aditya.sehgal@somaiya.edu>
2019-10-09 18:04:59 +0530

    complete todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import {trigger, style, animate, transition} from '@angular/animations';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'App1';
+  title = 'Application-1';
+
+  todoValue:string;
+  list:Todo[];
+  constructor(){
+    console.log("Constructor called");
+  }
+
+  ngOnInit(){
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem(){
+    if(this.todoValue !== "")
+    {
+      const newEntry: Todo = {
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newEntry);
+    console.log(this.list);
+    }
+    this.todoValue="";
+  }
+
+  deleteItem(id:number){
+    this.list=this.list.filter( item => item.id!==id );
+  }
 }
